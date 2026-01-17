aec6022e5c80b344c0dee41be33d8bbb69611875
BlackcoinDev <dev@blackcoin.dev>
2026-01-17 12:43:47 +0100

    fix(linter): Add noqa directives to imports

--- Bitmessage/bitmessageqt/iconglossary.py
+++ Bitmessage/bitmessageqt/iconglossary.py
@@ -99,8 +99,7 @@ class Ui_iconGlossaryDialog(object):
         )
 
 
-from . import bitmessage_icons_rc
-
+from . import bitmessage_icons_rc  # noqa: E402,F401
 if __name__ == "__main__":
     import sys
 
